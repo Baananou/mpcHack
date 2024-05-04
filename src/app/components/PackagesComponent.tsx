e4b61b57198b53b9649bd5cc86d371781e0fdb10
baananou <bananoumohamed30@gmail.com>
2024-05-04 23:23:21 +0200

    fixed

--- src/app/components/PackagesComponent.tsx
+++ src/app/components/PackagesComponent.tsx
@@ -1,7 +1,7 @@
 export const PackagesComponent = () => {
 	return (
 		<div className="mt-28">
-			<h1 className="text-center text-6xl max-sm:text-5xl font-bold">Prices</h1>
+			<h1 className="text-center text-6xl max-sm:text-5xl font-bold">Prizes</h1>
 			<div className="flex sm:space-x-4 max-sm:space-y-4 max-sm:flex-col text-center">
 				<div className="flex-1 text-xl mt-14 rounded-xl border border-[#F7E16F]/25 bg-[#19170d] p-10 w-full">
 					<div className="text-[#F7E16F]">First Place</div>
